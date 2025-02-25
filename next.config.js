a2f0729afcd97d4abfa20c0cca73dc4eaf981836
juanpaRdeCh <juanpablo.ruiz@live.com>
2025-02-25 13:28:18 +0100

    new name

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+// const nextConfig = {
+//   output: "export",  // Required for static export
+//   images: {
+//     unoptimized: true,  // Fixes issues with Next.js Image component on static hosting
+//   },
+//   basePath: '',  // Add this line
+  
+// };
+
+// module.exports = nextConfig;
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",  // Required for static export
   images: {
-    unoptimized: true,  // Fixes issues with Next.js Image component on static hosting
+    unoptimized: true,  // Fixes Next.js Image component for static hosting
   },
-  basePath: '',  // Add this line
-  
+  basePath: isProd ? "/juanpardech_portfolio" : "",  // Set repository name here
+  assetPrefix: isProd ? "/juanpardech_portfolio" : "",  // Fixes static file paths
 };
 
 module.exports = nextConfig;
